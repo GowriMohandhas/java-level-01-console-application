bbe8c7e409c06552b7d8ac73f18b93f181993ead
GowriMohandhas <gowripriya.m2007@gmail.com>
2026-05-09 16:38:10 +0530

    prgm 4

---
+++ Prime.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+class Prime
+{
+    public static void main(String args[]) throws Exception
+    {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.println("Prime Number Checking");
+        System.out.println("---------------------");
+        System.out.println("Enter the Number:");
+
+        String s1 = br.readLine();
+        int n = Integer.parseInt(s1);
+
+        System.out.println("Report");
+
+        int count = 0;
+
+        for(int i = 2; i <= n - 1; i++)
+        {
+            if(n % i == 0)
+            {
+                count++;
+            }
+        }
+
+        if(count == 0 && n > 1)
+        {
+            System.out.println("The number " + n + " is a Prime");
+        }
+        else
+        {
+            System.out.println("The number " + n + " is not a Prime");
+        }
+    }
+}
